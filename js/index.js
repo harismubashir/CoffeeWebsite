5e9d2db154302087f3cc70ffd879ed5d06cce6c9
harismubashir <harismubashir@users.noreply.github.com>
2020-06-23 17:25:10 -0230

    Pushing final changes for Course

---
+++ js/index.js
@@ -0,0 +1,22 @@
+console.log("message","hello","hi");
+let firstName = "Haris";
+let lastName = "Mubashir";
+console.log(firstName,lastName);
+
+let mySkills = ["excel","analysis", "management"]
+
+mySkills[0]="Handsome";
+mySkills[3]="excel";
+
+
+let name = {
+    first: firstName,
+    last: lastName,
+    age: 50,
+    skills: mySkills
+}
+
+name.property="Hari";
+name.skills[4]="Communication";
+    
+console.log(name);
