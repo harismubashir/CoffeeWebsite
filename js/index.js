233d9f855a8dad1741d200d56d02b1bd5d9d1e96
harismubashir <harismubashir@users.noreply.github.com>
2020-06-30 17:53:04 -0230

    modified and improved java script

--- js/index.js
+++ js/index.js
@@ -1,24 +1,22 @@
+window.addEventListener('load', pageLoad);
+// page load to give time for html to load before jscript
+function pageLoad() {
+    const api_url = "https://api.tradingeconomics.com/markets/search/coffee?c=guest:guest&f=json";
+    let priceRequest = new XMLHttpRequest();
+    priceRequest.addEventListener("load", showPrice);
+    priceRequest.open("GET", api_url);
+    priceRequest.send();
+}
 
-const api_url= "https://api.tradingeconomics.com/markets/search/coffee?c=guest:guest&f=json";
-let priceRequest = new XMLHttpRequest();
-priceRequest.addEventListener("load",getPrice);
-priceRequest.open("GET",api_url);
-priceRequest.send();
-function getPrice(e)
-{
+function showPrice(e) {
     e.preventDefault();
-    todaysPrice(e);
-
-
-
-}
-
-function todaysPrice(e){
-  
-  console.log("$"+JSON.parse(e.target.response)[0].Last);
-  document.getElementById('cp').innerHTML = "$" +JSON.parse(e.target.response)[0].Last;
+    console.log("$" + JSON.parse(e.target.response)[0].Last);
+    console.log(typeof e.target.response);
+    let myarray = JSON.parse(e.target.response);
+    document.getElementById('cp').innerHTML = "$" + JSON.parse(e.target.response)[0].Last;
 
 }
+
 
 //getPRICE();
 
@@ -30,38 +28,44 @@ let email = document.getElementById("email");
 let message = document.getElementById("message");
 let alert = document.getElementById("alert");
 
-submit.addEventListener("click",submitForm);
+submit.addEventListener("click", submitForm);
 
 function submitForm(e) {
     e.preventDefault();
-    validate(name.value, email.value, message.value);
+    /*
+    if(validate(...)) {
+        sendEmail(...);
+    } else {
+        showError(...)
+    }
 
+    Add code to cleanup forms and send update message of "sent"
+    */
+    validate(name.value, email.value, message.value);
 }
 
-function validate(name,email,message) {
-        if (name== "" || email== "" || message== ""){
-            alert.innerHTML = "Please complete all fields to submit form.";
-        }else{
+function validate(name, email, message) {
+    if (name == "" || email == "" || message == "") {
+        alert.innerHTML = "Please complete all fields to submit form.";
+    } else {
 
-            emailUrl = "https://hooks.zapier.com/hooks/catch/5963107/o9gm325";
-            let emailObj = {
-                name: name,
-                email: email,
-                messsage: message,
-                to: "harismubashir@gmail.com"
-            };
-            let emailRequest = new XMLHttpRequest();
-            emailRequest.open("POST", emailUrl);
-            emailRequest.send(JSON.stringify(emailObj));
-        }
-
-    
+        emailUrl = "https://hooks.zapier.com/hooks/catch/5963107/o9gm325";
+        let emailObj = {
+            name: name,
+            email: email,
+            messsage: message,
+            to: "harismubashir@gmail.com"
+        };
+        let emailRequest = new XMLHttpRequest();
+        emailRequest.open("POST", emailUrl);
+        emailRequest.send(JSON.stringify(emailObj));
+    }
 }
 
 //Return to top code
 let toTopButton = document.getElementById("jumptotop");
 toTopButton.addEventListener("click", goToTop);
 
-function goToTop(){
-    window.scrollTo(0,0);
+function goToTop() {
+    window.scrollTo(0, 0);
 }
