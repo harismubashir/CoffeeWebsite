b64eef25019de2dac6291e66ef101906e86719ed
harismubashir <harismubashir@users.noreply.github.com>
2020-06-30 13:33:46 -0230

    Coffee price script added and home button

--- js/index.js
+++ js/index.js
@@ -1,13 +1,32 @@
 
 const api_url= "https://api.tradingeconomics.com/markets/search/coffee?c=guest:guest&f=json";
-
-async function getPRICE()
+let priceRequest = new XMLHttpRequest();
+priceRequest.addEventListener("load",getPrice);
+priceRequest.open("GET",api_url);
+priceRequest.send();
+function getPrice(e)
 {
-    const response = await fetch(api_url);
-    const price = await response.json();
-    console.log(price[0]);
-    document.getElementById('cp').textContent = price;
+    e.preventDefault();
+    todaysPrice(e);
+
+
 
 }
 
-getPRICE();
+function todaysPrice(e){
+  
+  console.log("$"+JSON.parse(e.target.response)[0].Last);
+  document.getElementById('cp').innerHTML = "$" +JSON.parse(e.target.response)[0].Last;
+
+}
+
+//getPRICE();
+
+
+
+let toTopButton = document.getElementById("jumptotop");
+toTopButton.addEventListener("click", goToTop);
+
+function goToTop(){
+    window.scrollTo(0,0);
+}
